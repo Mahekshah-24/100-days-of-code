ec6d572a3393dc5b059053888128584e5c9f8377
Mahekshah-24 <mahekshah934934@gmail.com>
2026-09-15 11:02:20 +0530

    Add program to find max and min in an array
    Implement a program to find the maximum and minimum elements in an array.

---
+++ day29q2.c
@@ -0,0 +1,55 @@
+Q58: Find the maximum and minimum element in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+2 9 1 4 7
+Output 1:
+Max=9, Min=1
+
+Input 2:
+3
+10 10 10
+Output 2:
+Max=10, Min=10
+
+*/
+  #include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int a[100];
+    int max, min;
+
+    scanf("%d", &n);
+
+    // Input array elements
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    // Assume first element is both maximum and minimum
+    max = a[0];
+    min = a[0];
+
+    // Find maximum and minimum
+    for(i = 1; i < n; i++)
+    {
+        if(a[i] > max)
+        {
+            max = a[i];
+        }
+
+        if(a[i] < min)
+        {
+            min = a[i];
+        }
+    }
+
+    printf("Max=%d, Min=%d", max, min);
+
+    return 0;
+}
